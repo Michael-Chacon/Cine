bcb9276ee18e3f9a5be70e81a4a256e255314fa9
Michael-Chacon <michaelchacon02@gmail.com>
2024-10-15 13:49:27 -0500

    feat: :sparkles: Se creó el CRUD para la entidad género, se implementaron los endpoints y se habilitó la consulta de géneros desde la API

--- src/main/java/com/cine/genre/domain/service/GenreService.java
+++ src/main/java/com/cine/genre/domain/service/GenreService.java
@@ -1,31 +1,43 @@
 package com.cine.genre.domain.service;
 
+import com.cine.genre.domain.repository.GenreRepository;
 import com.cine.genre.persistence.Genre;
+import com.cine.utils.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Set;
 
 @Service
-public class GenreService implements IGenre{
+@Transactional
+public class GenreService implements IGenre {
+  @Autowired GenreRepository repository;
 
-    @Override
-    public Optional<Genre> findById(Long id) {
-        return Optional.empty();
-    }
+  @Transactional(readOnly = true)
+  @Override
+  public Genre findById(Long id) {
+    return repository
+        .findById(id)
+        .orElseThrow(
+            () ->
+                new ResourceNotFoundException("El genero con el id " + id + " no esta registrado"));
+  }
 
-    @Override
-    public List<Genre> findAll() {
-        return null;
-    }
+  @Transactional(readOnly = true)
+  @Override
+  public List<Genre> findAll() {
+    return repository.findAll();
+  }
 
-    @Override
-    public Genre save(Genre genre) {
-        return null;
-    }
+  @Override
+  public List<Genre> save(Set<Genre> genre) {
+    return repository.saveAllAndFlush(genre);
+  }
 
-    @Override
-    public Genre delete(Long id) {
-        return null;
-    }
+  @Override
+  public void delete(Long id) {
+    repository.deleteById(id);
+  }
 }
