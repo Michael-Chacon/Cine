6cdef0081d08c0cf0c570acb1992db2f697af29a
Michael-Chacon <michaelchacon02@gmail.com>
2024-10-17 07:14:19 -0500

    Fix: :wrench: Se añadió configuración de acceso a la API de TMDB

--- src/main/java/com/cine/utils/Constants.java
+++ src/main/java/com/cine/utils/Constants.java
@@ -4,6 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class Constants {
   public static final String URL_BASE = "https://api.themoviedb.org/3";
-  @Value("${tmdb.api.token}")
-  public static String TOKEN;
+//  @Value("${tmdb.api.token}")
+  public static final String TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNTVjNTY3ZWQ0MjM4NjA2NWQ5NTk4ZGE0YmRmOWVjYyIsIm5iZiI6MTcyODk5Njc2Ni4wMDEwMzMsInN1YiI6IjY0ODdjZjMzZTM3NWMwMDBlMjUwYmUwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yIQwFTn0ONY_61OyCF5VNT-lW3uI88IrWTHOxoXJFoA";
 }
